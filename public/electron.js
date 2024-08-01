705108ef166ec2b948d299b844f6c59980a9dd1d
Raffa Bux <raffaelebussolotto@gmail.com>
2024-08-02 00:44:59 +0200

    confguration files update

--- public/electron.js
+++ public/electron.js
@@ -1,30 +1,34 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
 
-function createWindow () {
+require('@electron/remote/main').initialize()
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
   })
 
-  win.loadURL('http://localhost:3000');
-  // win.loadFile('../src/index.js');
-
-  win.webContents.openDevTools()
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
 }
 
-app.whenReady().then(createWindow)
+app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
+app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
